0be3dea19cd1d5026856ffc395d9107be8599b2d
Chriss Okoth <okothchris15@gmail.com>
2022-10-15 02:43:21 -0700

    printing new progam using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	//string initialisation
 	char Mystr[] = "Programming is like building a multilingual puzzle";
-	puts(Mystr); //writing the string to stdout
+	puts(Mystr);
 	return 0;
 }
