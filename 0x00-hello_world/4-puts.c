00a407939b097c598772b9451000b29a79201624
Chriss Okoth <okothchris15@gmail.com>
2022-10-15 02:38:40 -0700

    printing a C program using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	//string initialisation
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+	puts(Mystr); //writing the string to stdout
+	return 0;
+}
