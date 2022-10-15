aa3c8e1cb135bdcd4e1d7a6689117ad98aca57d2
Chriss Okoth <okothchris15@gmail.com>
2022-10-15 02:47:15 -0700

    fixing the puts program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr);
-	return 0;
+
+	return [0];
 }
