ac19163e53d32026c6d4a449e3e55c470ad65e3a
Chriss Okoth <okothchris15@gmail.com>
2022-10-16 16:20:39 -0700

    removing whitespace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,11 +5,8 @@
 #include <stdio.h>
 
 
-
-/* 
- * main - Entry point
- *
- * Description- prints the last digit of randomly genrated numbers
+/**
+ *main - Entry point- Prints the last digit of randomly genrated numbers
  *
  *
  *Return: Always 0 (success)
@@ -31,24 +28,15 @@ int main(void)
 				{
 					
 					printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-
 				}
-
-				else if (( n % 10) < 6 && (n % 10) != 0)
+				else if ((n % 10) < 6 && (n % 10) != 0)
 				{
-					
 					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-
 				}
-
 				else
 				{
-
 					printf("Last digit of %d is %d and is 0\n", n, n % 10);
-
 				}
 
-
 					return (0);
-
 }
