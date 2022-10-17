25b13d55725d9e140b9e2e38205d484d14228f78
Chriss Okoth <okothchris15@gmail.com>
2022-10-17 02:20:50 -0700

    removing triling whitespace

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,20 @@
 int main(void)
 
 {
-	char letter;	
+	char letter;
+
+
 	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
 		putchar(letter);
+	}
 	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
 		putchar(letter);
+	}
 	putchar('\n');
+
 	return (0);
 
 }
