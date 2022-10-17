3b3ecd7ef121eff241bc8373686ddf0aa8346f80
Chriss Okoth <okothchris15@gmail.com>
2022-10-17 05:39:41 -0700

    fixing whitespace

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,12 +12,14 @@ int main(void)
 {
 	int digit;
 
-	for (digit == '0'; digit <= '9'; digit++)
+	for (digit = '0'; digit <= '9'; digit++)
 	{
 		putchar(digit);
-		if (digit == '9')
+
+		if (digit = '9')
 		{
 			digit = 'a';
+
 			for (; digit <= 'f'; digit++)
 			{
 				putchar(digit);
