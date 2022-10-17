0c668212c42f0235aec50bc7947d8aef4dbecb65
Chriss Okoth <okothchris15@gmail.com>
2022-10-16 17:04:23 -0700

    removing whitespace

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point- prints alphabet in lowercase then uppercase
  *
@@ -8,16 +10,12 @@
 int main(void)
 
 {
-	char letter;
-	
+	char letter;	
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
-	
 	putchar('\n');
-	
 	return (0);
 
 }
