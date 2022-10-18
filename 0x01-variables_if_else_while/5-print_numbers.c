b6ea6e2ea67d6cc23f458115e411a1346e06584a
Chriss Okoth <okothchris15@gmail.com>
2022-10-18 03:12:48 -0700

    fixing whtespace

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,13 +8,13 @@ int main(void)
 
 {
 	int digit;
-	
-	for (digit = '0'; digit <= '9'; digit++)
 
+	for (digit = '0'; digit <= '9'; digit++)
+	
 	{
 		putchar(digit);
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
