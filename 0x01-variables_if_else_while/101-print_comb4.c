63515d080891232ec415f801360ca64e389787d9
Chriss Okoth <okothchris15@gmail.com>
2022-10-19 04:48:49 -0700

    fix refator issues

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,35 @@
 #include<stdio.h>
 
 /**
- * main - Entry point. Prints all possible different combinations of three digits
+ * main - Entry point.
  *
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-	int digit_one, digit_two, digit_three;
+	int a,b,c;
 
-	for (digit_one = '0'; digit_one <= '9'; digit_one++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (digit_two = '0'; digit_two <= '9'; digit_two++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (digit_three = '0'; digit_three <= '9'; digit_three++)
+			for (c = '0'; c <= '9'; c++)
 			{
-				if (digit_one != digit_two && digit_one < digit_two)
-					
-					if (digit_two != digit_three && digit_two < digit_three)
-					{
-						putchar(digit_one);
-						putchar(digit_two);
-						putchar(digit_three);
+				if ((a != b && a < b)&& (b != c && b < c))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
 
-						if (digit_one == '7' && digit_two == '8' && digit_three == '9')
-							continue;
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					if (a == '7' && b == '8' && c == '9')
+						continue;
+					else
+					{
+						putchar(',');
+						putchar(' ');
 					}
+				}
 			}
 		}
 	}
