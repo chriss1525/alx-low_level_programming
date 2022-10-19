81a2a84add35be841966fd633a352a773a7744e8
Chriss Okoth <okothchris15@gmail.com>
2022-10-19 01:02:30 -0700

    printing all numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -17,17 +17,14 @@ int main(void)
 	for (digit = '0'; digit <= '9'; digit++)
 	{
 		putchar(digit);
-
-		if (digit = '9')
-		{
-			digit = 'a';
-
-			for (; digit <= 'f'; digit++)
-			{
-				putchar(digit);
-			}
-		}
 	}
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+
+	{
+		putchar(digit);
+	}
+
 	putchar('\n');
 
 	return (0);
