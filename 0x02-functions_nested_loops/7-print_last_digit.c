5c26ae61f02dd06ab784622fbfdefc0bd178363e
Chriss Okoth <okothchris15@gmail.com>
2022-10-22 02:35:34 +0300

    add if statement

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,16 @@
 */
 int print_last_digit(int n)
 {
-	int r = _abs(n % 10);
+	int r = (n % 10);
 
-	_putchar(r + '0');
+	if (n < 0)
+	{
+		_putchar('0' - r);
+	}
+	else
+	{
+		_putchar(r + '0');
+	}
+
 	return (r);
 }
