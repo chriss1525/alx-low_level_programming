283d2e75764087ce55825b878a1dd84d83608ad0
Chriss Okoth <okothchris15@gmail.com>
2022-10-22 20:45:27 +0300

    print numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,72 @@
+#include "main.h"
+/**
+ *print_to_98 - prints to 98
+ *
+ *@n: int
+ *
+ *Return: void
+*/
+void print_to_98(int n)
+{
+	int i = 98;
+
+	if (i > n)
+	{
+		for (i, n; i - n >= 0; n++)
+		{
+			if (n <= 9)
+			{
+				if (n < 0 && n > -10)
+				{
+					_putchar('-');
+					_putchar((n * -1) + '0');
+				}
+				else if (n <= -10)
+				{
+					_putchar('-');
+					_putchar(((n * -1) / 10) + '0');
+					_putchar(((n * -1) % 10) + '0');
+				}
+				else
+				{
+					_putchar(n + '0');
+				}
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i, n; i - n <= 0; n--)
+		{
+			if (n >= 100)
+			{
+				_putchar((n / 100) + '0');
+				_putchar(((n % 100) / 10) + '0');
+				_putchar(((n % 100) % 10) + '0');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar(10);
+}
