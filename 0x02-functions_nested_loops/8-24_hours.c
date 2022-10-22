c4e5508178ee12601e428c48c6f009af008e9419
Chriss Okoth <okothchris15@gmail.com>
2022-10-22 12:00:46 +0300

    print all the minutes of the day by jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *jack_bauer - prints every minute of the day by Jack bauer
+ *
+ *
+ *Return: void
+*/
+void jack_bauer(void)
+{
+	int min;
+	int hour;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar(hour + '0');
+			_putchar(min + '0');
+		}
+		_putchar(hour + '0');
+		_putchar(min + '0');
+	}
+}
