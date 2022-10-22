51344f1f9e8b3f28ca1ad7a10c0bfb8e51ea00ee
Chriss Okoth <okothchris15@gmail.com>
2022-10-22 12:24:01 +0300

    adding putchar statement

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,12 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
+			_putchar(10);
 		}
-		_putchar(':');
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
 	}
 }
