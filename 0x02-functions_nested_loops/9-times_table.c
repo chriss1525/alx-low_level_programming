31b861fc20b2022d13e42d1d18d575bb1b982e5c
Chriss Okoth <okothchris15@gmail.com>
2022-10-22 16:13:05 +0300

    add new char

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 {
 	int n;
 	int i;
-	int c = n * i;
+	int c;
 
 	for (n = 0; n <= 10; n++)
 	{
@@ -21,6 +21,8 @@ void times_table(void)
 		_putchar(',');
 		_putchar(' ');
 	}
+	c = n * i;
+	
 	if (c <= 9)
 	{
 		_putchar(' ');
