4a004b10cb93a2d6a20b1a5ccad30193409e5e46
Chriss Okoth <okothchris15@gmail.com>
2022-10-22 23:44:56 +0300

    add space

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,19 +5,19 @@
  *@n: int
  *
  *Return: void
-*/
+ */
 void print_times_table(int n)
 {
-    int x;
+	int x;
 	int i;
 	int c;
-
+	
 	for (x = 0; x <= n; x++)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			c = x * i;
-
+			
 			if (c <= 9)
 			{
 				_putchar(c + '0');
@@ -27,15 +27,18 @@ void print_times_table(int n)
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 			}
+			
 			if (x * (i + 1) <= n && i != n)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 			}
 			else if (i != n)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 			}
 		}
