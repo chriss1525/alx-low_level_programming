1ba860c3d89dc74744d7c45e8ea29853d787d82e
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 17:37:07 +0300

    fix error

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point, prints the first fifty fibonnanci numbers
+ *
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+    int n;
+    int i[] = {n, n++};
+    for (n = 0; n < 100; n++)
+    {
+        printf("%d, ", i, i);
+        return 0;
+    }
+    printf("\n");
+}
