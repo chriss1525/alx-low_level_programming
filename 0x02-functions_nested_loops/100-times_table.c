012290c6042467858a584f02cebb95015a54cc7d
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 12:00:37 +0300

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,44 +11,43 @@ void print_times_table(int n)
 	int x, i, c;
 
 	if (n <= 0 && n >= 15)
+		return;
+	for (x = 0; x <= n; x++)
 	{
-		for (x = 0; x <= n; x++)
+		for (i = 0; i <= n; i++)
 		{
-			for (i = 0; i <= n; i++)
+			c = x * i;
+
+			if (c == 0)
 			{
-				c = x * i;
-
-				if (c == 0)
-				{
-					_putchar(c + '0');
-				}
-
-				if (c < 10 && i != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(c + '0');
-				}
-				else if (c >= 10 && c < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((c / 100) + '0');
-					_putchar(((c / 10) % 10) + '0');
-					_putchar((c % 10) + '0');
-				}
+				_putchar(c + '0');
 			}
-			_putchar('\n');
+
+			if (c < 10 && i != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (c >= 10 && c < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 100) + '0');
+				_putchar(((c / 10) % 10) + '0');
+				_putchar((c % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
