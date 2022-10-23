d9f73d436abcfbc1dcf6ed745b5688e65dce6608
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 16:19:10 +0300

    introduce while loop

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,20 @@
 int main(void)
 {
 	int n;
-	int sum = 0;
-
-	for (n = 0; n < 1024; n++)
+    int i = 0;
+	
+	while (n < 1024)
 	{
-		if (n % 3 == 0 || n % 5 == 0)
-		{
-			sum += n;
-			printf("%d, ", sum);
-		}
+    	if (n % 3 == 0)
+        {
+        i += n;
+        }
+        else if (n % 5 == 0)
+        {
+        i += n;
+        }
+        n++;
 	}
-	printf("\n");
-	return (0);
+    printf("%d\n", i);
+    return (0);
 }
