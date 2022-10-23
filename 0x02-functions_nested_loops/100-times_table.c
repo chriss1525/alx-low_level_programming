7e3c6e6a08e51113bf2137d7ae9a7d6c0f19ef46
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 12:19:26 +0300

    comment out code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,8 +31,8 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(c + '0');
 			}
-			else 
-			/*if (c >= 10 && c < 100)*/
+			/*else 
+			if (c >= 10 && c < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -40,7 +40,7 @@ void print_times_table(int n)
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 			}
-			/* else
+			else
 			{
 			_putchar(',');
 			_putchar(' ');
