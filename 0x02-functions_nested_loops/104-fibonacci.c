ea2effd8bf05ed479d80c2274b8422fb7ec51f75
Chriss Okoth <okothchris15@gmail.com>
2022-10-24 00:38:33 +0300

    program that prints 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ *
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long int n = 98;
+	long int a = 1;
+	long int b = 2;
+	long int c;
+	long int d = 0;
+
+	printf("%ld, %ld", a, b);
+
+	while (d < n - 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf(", %ld", c);
+		d++;
+	}
+	printf("\n");
+
+	return (0);
+}
