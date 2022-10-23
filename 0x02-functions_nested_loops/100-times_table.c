0b37d1df6d84a4cf9a95365b6bc9bebb4e2cd0b2
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 12:17:09 +0300

    fix else statement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 				_putchar(c + '0');
 			}
 			else 
-			// if (c >= 10 && c < 100)
+			/*if (c >= 10 && c < 100)*/
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -40,14 +40,14 @@ void print_times_table(int n)
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 			}
-			// else
-			// {
-			// 	_putchar(',');
-			// 	_putchar(' ');
-			// 	_putchar((c / 100) + '0');
-			// 	_putchar(((c / 10) % 10) + '0');
-			// 	_putchar((c % 10) + '0');
-			// }
+			/* else
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((c / 100) + '0');
+			_putchar(((c / 10) % 10) + '0');
+			_putchar((c % 10) + '0');
+			}*/
 		}
 		_putchar('\n');
 	}
