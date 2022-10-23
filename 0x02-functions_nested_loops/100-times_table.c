55154e7878a6d7427d2cbf90011052acc64c0f85
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 22:35:17 +0300

    print n times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,8 +41,8 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((c / 100) + '0');
-					_putchar((c % 10) + '0');
 					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
 				}
 			}
 			_putchar('\n');
