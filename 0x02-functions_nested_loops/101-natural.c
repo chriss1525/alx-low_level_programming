f994d3e790549632d955c5dac410149de1d44359
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 16:05:41 +0300

    compile the sum of multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of all multiples of 3 or 5
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n >= 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+			printf("%d, ", sum);
+		}
+	}
+	printf("\n");
+	return (0);
+}
