60d8d8a74213d5e82f5732b711657729ef119233
Chriss Okoth <okothchris15@gmail.com>
2022-10-23 21:29:31 +0300

    print fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,20 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - entry point, prints the first fifty fibonnanci numbers
+ * printFibonacci - entry point, prints the first fifty fibonnanci numbers
+ *
+ *main - entry to recurssion
+ *
+ *
+ *@n: int
  *
  *
  * Return: 0 (success)
  */
-int main(void) 
+#include <stdio.h>
+void printFibonacci(int n)
 {
-	int n;
-    int i[50] = {n, n++};
-    for (;n > 0; n++)
-    {
-    	printf("%d, ", i,i);
-        return 0;
+	int a = 1;
+	int b = 2;
+	int c;
+
+	if (n > 0)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%d ", c);
+		printFibonacci(n - 1);
+
+		int main(void)
+		{
+			int n;
+
+			printf("50");
+			scanf("%d", n);
+			printf("Fibonacci Series: ");
+			printf("%d %d ", 0, 1);
+			printFibonacci(n - 2);
+
+			return (0);
+		}
 	}
-    printf("\n");
 }
