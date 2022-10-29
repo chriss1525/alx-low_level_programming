28cb8bfc9f95368310960e4dfb5d8b02a06b1f71
Chriss Okoth <okothchriss15@gmail.com>
2022-10-29 12:28:17 +0300

    fix betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string to print
  */
 void print_rev(char *s)
 {
-    int i, c, x;
+	int i, c, x;
 
-    i = '0'; 
-    while (s[i] != '\0')
-    {
-        i++;
-    }  
+	i = '0';
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-    x = i;
+	x = i;
 
-    for (c = x-i; c >= '0'; c--)
-    {
-        _putchar(s[c]);
-    }
+	for (c = x - i; c >= '0'; c--)
+	{
+		_putchar(s[c]);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
