21cb0a90eb48dfef8778dc25f29da3522f0f224a
Chriss Okoth <okothchriss15@gmail.com>
2022-10-30 20:04:38 +0300

    print all digits apart from 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,13 +12,12 @@ void print_most_numbers(void)
 	for (n = 0; n <= 9; n++)
 	{
 		if (n != 2 && n != 4)
-		{
-			_putchar(n + '0');
-		}
-		else
-		{
-		}
-
-		_putchar('\n');
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+			}
 	}
+	_putchar('\n');
 }
