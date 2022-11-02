9a260c13a2a58528ac7e5367bda886a4ab85eb08
Chriss Okoth <okothchriss15@gmail.com>
2022-11-02 22:20:32 +0300

    print numbers ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,15 @@ void more_numbers(void)
 	{
 		for (i = 0; 1 <= 14; i++)
 		{
+			if (i < 10)
+			{
 			_putchar (i + '0');
+			}
+			else
+			{
+				_putchar ((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
