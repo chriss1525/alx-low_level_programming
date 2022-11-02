33c50b7bbeadfad8511f7dce89c90424d2f98545
Chriss Okoth <okothchriss15@gmail.com>
2022-11-03 00:47:29 +0300

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,29 @@ int main(void)
 	int n;
 
 	for (n = 1; n <= 100; n++)
-	{
-		if (n % 15 == 0)
+	{if (n != 100)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (n % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (n % 5 == 0)
-		{
-			printf("Buzz ");
+
+			if (n % 15 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (n % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (n % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", n);
+			}
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("Buzz");
 		}
 	}
 	printf("\n");
