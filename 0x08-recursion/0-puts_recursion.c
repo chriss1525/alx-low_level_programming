469cf79f16fae04073f398c2c4619b9b83db777c
Chriss Okoth <okothchriss15@gmail.com>
2022-11-04 04:38:44 +0300

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts_recursion - prints string
+ *@s: string
+ *
+ * Return:  void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
