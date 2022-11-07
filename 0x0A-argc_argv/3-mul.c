88d9fe3f454171ae5540a2e8ee541f2de4b9f84a
Chriss Okoth <okothchriss15@gmail.com>
2022-11-08 00:11:12 +0300

    multiply two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,25 +6,25 @@
  * @argv: string. argument array
  * atoi: turns string to int
  * Return: 0 (success)
-*/
+ */
 int main(int argc, char *argv[])
 {
-    int i;
-    int n;
-    int c;
+	int i;
+	int n;
+	int c;
 
-    if (argc > 2)
-    {
-        i = atoi(argv[1]);
-        n = atoi(argv[2]);
-        c = i * n;
+	if (argc > 2)
+	{
+		i = atoi(argv[1]);
+		n = atoi(argv[2]);
+		c = i * n;
 
-        printf("%d\n", c);
-        return (0);
-    }
-    else
-    {
-        printf("Error\n");
-    }
-    return (1);
+		printf("%d\n", c);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (1);
 }
