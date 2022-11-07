dab96be2a02aec5afee59795d53e17f0055a3d6e
Chriss Okoth <okothchriss15@gmail.com>
2022-11-07 22:09:56 +0300

    check if number is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * divisor - entry point. determiner
+ * 
+ * @n: int
+ * 
+ * Return: 1 if true and 0 if false
+*/
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+		return (0);
+	else if (m / 2 > n)
+		return (divisors(n + 2, m));
+	else
+		return (1);
+}
+
+/**
+ * is_prime_number - entry point. checks if number is a prime number
+ * @n: int
+ * Return: 0 True or 1 False
+ */
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+		return (0);
+	else
+		return (divisors(3, n));
+}
