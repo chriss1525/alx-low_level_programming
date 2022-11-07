fd1a9cc2a843ee1b2942329027cd47d6c968a799
Chriss Okoth <okothchriss15@gmail.com>
2022-11-07 23:50:21 +0300

    print passed arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,18 +4,12 @@
  * @argc: argument count
  * @argv: array of arguments passed
  * Return: 0 (success)
-*/
+ */
 int main(int argc, char *argv[])
 {
-    int i;
-
-    if (argc > 1)
-    {
-        while (i < argc)
-    {
-        printf("%s\n", argv[i]);
-        i++;
-    }
-    }
-    return (0);
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
 }
