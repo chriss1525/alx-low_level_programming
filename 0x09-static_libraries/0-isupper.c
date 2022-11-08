c5284ee51b7e2c65b2037632f7fa95ecf9fbb759
Chriss Okoth <okothchriss15@gmail.com>
2022-11-08 16:13:39 +0300

    create libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isupper - Entry poit. Checks if character is lower
+ *
+ * @c: int
+ *
+ *Return: 1 (success)
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
