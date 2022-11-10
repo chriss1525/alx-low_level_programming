22323059439f285ceb363ff75e64ff02ef297182
chriss1525 <okothchris15@gmail.com>
2022-11-10 23:48:44 +0300

    fix spacing

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,23 +7,23 @@
  */
 void puts_half(char *str)
 {
-    int len, i;
+	int len, i;
 
-    len = strlen(str);
+	len = strlen(str);
 
-    if (len % 2 == 0)
-    {
-        for (i = len / 2; i < len; i++)
-        {
-            _putchar(*(str + i));
-        }
-    }
-    else
-    {
-        for (i = (len + 1) / 2; i < len; i++)
-        {
-            _putchar(*(str + i));
-        }
-    }
-    _putchar('\n');
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	else
+	{
+		for (i = (len + 1) / 2; i < len; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	_putchar('\n');
 }
