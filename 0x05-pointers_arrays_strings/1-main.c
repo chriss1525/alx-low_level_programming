4228fc1db7b98eee068ff852831aed91f2de8adc
chriss1525 <okothchris15@gmail.com>
2022-11-14 23:38:54 +0300

    copy string from one pointer to another

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -1,19 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code for
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int array[5];
+    char s1[98];
+    char *ptr;
 
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
+    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+    printf("%s", s1);
+    printf("%s", ptr);
     return (0);
 }
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * strcpy = copies a string from source to destination
+ * @src: source pointer
+ * @dest: new pointer destination
+ * Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+    int i;
+    for (i = 0; src[i] != '\0'; i++)
+    {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+    return dest;
+}
