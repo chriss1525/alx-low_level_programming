1c729d779cf04a4b01fa5d9790ea5d6d7ec6b8b4
chriss1525 <okothchris15@gmail.com>
2022-11-16 02:59:43 +0300

    initialize variable type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variabe of tpe struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: describes dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+
+		d == malloc(sizeof(struct dog));
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,6 @@ struct dog
 	char *owner;
 };
 
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /*DOG_H*/
