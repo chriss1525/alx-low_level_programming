7e1005ef276c5faf85b38808963ad2eea8abc854
chriss1525 <okothchris15@gmail.com>
2022-11-16 02:41:19 +0300

    defining struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+#0x0E-structures_typedef
+Learnig how to define structures in C programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines dog macro
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: describes dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
