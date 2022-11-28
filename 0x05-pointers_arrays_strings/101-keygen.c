be073be0549d43e228e1433d730d10f954ac27cb
chriss1525 <okothchris15@gmail.com>
2022-11-28 13:37:39 +0300

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+/**
+ * main - Entry point. randomizes numbers
+ * 
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+    int i, j, n;
+	char *pass;
+	
+	j = 0;
+	
+	srand(time(NULL));
+	
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		j += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - j) - '0' < 78)
+		{
+			n = 2772 - j - '0';
+			j += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+    return(0);
+}
