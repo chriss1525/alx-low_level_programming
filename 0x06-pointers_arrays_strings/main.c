3e969d3094117cc72e6624ababd8c32af52225bf
chriss1525 <okothchris15@gmail.com>
2022-11-28 15:36:43 +0300

    concatenate two strings

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -14,7 +14,7 @@ int main(void)
 
     printf("%s\n", s1);
     printf("%s", s2);
-    ptr = _strcat(s1, s2);
+    ptr = _strncat(s1, s2, 1024);
     printf("%s", s1);
     printf("%s", s2);
     printf("%s", ptr);
