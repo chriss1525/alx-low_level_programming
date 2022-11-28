32679452fba101cd753bf3bdc8e8ea78bf373dc7
chriss1525 <okothchris15@gmail.com>
2022-11-28 15:49:47 +0300

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of items in src string
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
