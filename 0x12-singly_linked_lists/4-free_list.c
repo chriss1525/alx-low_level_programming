ee17b22b836c7484f5bd25f114d5197332839e0a
chriss1525 <okothchris15@gmail.com>
2022-12-02 02:07:29 +0300

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: node
+*/
+
+void free_list(list_t *head)
+{
+    list_t *str2;
+
+	while (head)
+	{
+		str2 = head->next;
+		free(head->str);
+		free(head);
+		head = str2;
+	}
+}
