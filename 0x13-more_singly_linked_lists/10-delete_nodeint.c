ddc4bba0ac21fb62375dd18a86e3d3791263f6c0
chriss1525 <okothchris15@gmail.com>
2022-12-08 15:15:43 +0300

    delete node at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes a specific node at an index
+ * @head: list of integers
+ * @index: deleted index
+ * Return: 1 (success) -1 (fail)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp;
+	listint_t *new;
+	temp = *head;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		new = (*head)->next;
+		free(*head);
+		*head = new;
+		return (1);
+	}
+	while (i < (index - 1))
+	{
+		if ((temp->next) == NULL)
+		{
+			return (-1);
+		}
+		temp = temp->next;
+		i++;
+	}
+	new = temp->next;
+	temp->next = new->next;
+	free(new);
+	return (1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *insert_node_at_index - inserts new node at specific index
+ *insert_nodeint_at_index - inserts new node at specific index
  *@head: linked list
  *@idx: specified index
  *@n: value of node
@@ -8,38 +8,38 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new;
-    listint_t *temp;
-    unsigned int i = 0;
+	listint_t *new;
+	listint_t *temp;
+	unsigned int i = 0;
 
-    new = malloc(sizeof(listint_t));
-    temp = *head;
+	new = malloc(sizeof(listint_t));
+	temp = *head;
 
-    /*printf("%u", i);*/
+	/*printf("%u", i);*/
 
-    new->n = n;
+	new->n = n;
 
-    if (idx == 0)
-    {
-        new->next = temp;
-        temp = new;
-        return (new);
-    }
+	if (idx == 0)
+	{
+		new->next = temp;
+		temp = new;
+		return (new);
+	}
 
-    if (!new || !head)
+	if (!new || !head)
 		return (NULL);
 
-    while (i < idx)
-    {
-        if (i == (idx - 1))
-        {
-            new->next = temp->next;
-            temp->next = new;
-        }
+	while (i < idx)
+	{
+		if (i == (idx - 1))
+		{
+			new->next = temp->next;
+			temp->next = new;
+		}
 
-        temp = temp->next;
-        i++;
-    }
+		temp = temp->next;
+		i++;
+	}
 
-    return (new);
+	return (new);
 }
