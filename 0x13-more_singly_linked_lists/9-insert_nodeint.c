4abb1e10d3f0325f6fd163cf89ef5c03d1cb13b5
chriss1525 <okothchris15@gmail.com>
2022-12-08 15:50:47 +0300

    add new node to list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,14 +15,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	temp = *head;
 
-	/*printf("%u", i);*/
-
 	new->n = n;
 
 	if (idx == 0)
 	{
-		new->next = temp;
-		temp = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
 
@@ -33,8 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == (idx - 1))
 		{
-			new->next = temp->next;
-			temp->next = new;
+			new->next = temp;
+			temp = new;
 		}
 
 		temp = temp->next;
