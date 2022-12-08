b9d8ac229c23dbdcc3cc521c2e82ccb6cf57ce78
chriss1525 <okothchris15@gmail.com>
2022-12-08 15:24:38 +0300

    delete node at nth index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	temp = *head;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
@@ -25,11 +25,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (i < (index - 1))
 	{
-		temp = temp->next;
 		if ((temp->next) == NULL)
 		{
 			return (-1);
 		}
+		temp = temp->next;
 		i++;
 	}
 	new = temp->next;
