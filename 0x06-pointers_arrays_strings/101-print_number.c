44db798afc61961e304a2ce88ccebbe466bdcac1
chriss1525 <okothchris15@gmail.com>
2022-12-08 17:39:07 +0300

    fix betty styling

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- *
+ * print_number - prints integer
+ * @n: integer
  */
 void print_number(int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = n * -1;
-    }
-    
-    i = n;
-    if (i / 10)
-        print_number(i / 10);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
 
-    _putchar(i % 10 + '0');
+	i = n;
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
 }
