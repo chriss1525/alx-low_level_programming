fcccf41bd73065407489f703b720e7809fcaf1bf
chriss1525 <okothchris15@gmail.com>
2022-12-08 13:35:15 +0300

    add null considerations

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
         return (new);
     }
 
-    if (head == NULL)
-        return (NULL);
+    if (!new || !head)
+		return (NULL);
 
     while (i < idx)
     {
