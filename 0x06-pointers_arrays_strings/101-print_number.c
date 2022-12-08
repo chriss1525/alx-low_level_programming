7a4f1a2886754862f04ef3c511e4ad9c3f5f82a9
chriss1525 <okothchris15@gmail.com>
2022-12-08 17:38:00 +0300

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ */
+void print_number(int n)
+{
+    unsigned int i;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        n = n * -1;
+    }
+    
+    i = n;
+    if (i / 10)
+        print_number(i / 10);
+
+    _putchar(i % 10 + '0');
+}
