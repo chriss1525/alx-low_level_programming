ea57cca45006c6579a2f22f6be3d120e6c943389
chriss1525 <okothchris15@gmail.com>
2022-12-28 16:07:43 +0300

    fix betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,19 +8,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    int i;
-    int j;
-    unsigned int length;
+	int i;
+	int j;
+	unsigned int length;
 
-    length = 0;
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
-            continue;
-        if (s[i] == accept[j])
-            length++;
-        if (accept[j] == '\0')
-            return (length);
-    }
-    return (length);
+	length = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
+			continue;
+		if (s[i] == accept[j])
+			length++;
+		if (accept[j] == '\0')
+			return (length);
+	}
+	return (length);
 }
