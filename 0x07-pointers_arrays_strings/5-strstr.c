ff4fdb63634f2f4f98b20a3fba317ee81d223c0c
chriss1525 <okothchris15@gmail.com>
2023-01-02 20:11:15 +0300

    fix betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,26 +8,26 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    char *h = haystack;
-    char *n = needle;
+	char *h = haystack;
+	char *n = needle;
 
-    while (*h)
-    {
-        n = needle;
-        h = haystack;
-        while (*n)
-        {
-            if (*h == *n)
-            {
-                n++;
-                h++;
-            }
-            else
-                break;
-        }
-        if (*n == '\0')
-            return (haystack);
-        haystack++;
-    }
-    return (0);
+	while (*h)
+	{
+		n = needle;
+		h = haystack;
+		while (*n)
+		{
+			if (*h == *n)
+			{
+				n++;
+				h++;
+			}
+			else
+				break;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
 }
