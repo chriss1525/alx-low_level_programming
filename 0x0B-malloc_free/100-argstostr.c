45a8d51ff608d2234e1c8729e11975bd485dc261
chriss1525 <okothchris15@gmail.com>
2023-01-15 11:09:48 +0300

    concatenate alla aguments of a program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,42 +1,43 @@
 #include "main.h"
 #include <string.h>
 /**
- * argstostr - concatenates all arguments
+ * argstostr - concatenates all the arguments of your program
  * @ac: count of arguments passed
- * @av: array of arguments
+ * @av: string of arguments passed
+ * Return: pointer to new string (success)
 */
 
 char *argstostr(int ac, char **av)
 {
-		char *new = NULL;
-		int k = 0;
-        int i = ac;
-        int j;
-        int sum = 0;
-        int temp = 0;
+	char *new = NULL;
+	int k = 0;
+	int i = ac;
+	int j;
+	int sum = 0;
+	int temp = 0;
 
-		if (ac == 0 || av == NULL)
-			return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-		while (ac--)
-			sum += (strlen(av[ac]) + 1);
-		new = (char *) malloc(sum + 1);
+	while (ac--)
+		sum += (strlen(av[ac]) + 1);
+	new = (char *)malloc(sum + 1);
 
-		if (new != NULL)
+	if (new != NULL)
+	{
+		while (k < i)
 		{
-			while (k < i)
-			{
-				for (j = 0; av[k][j] != '\0'; j++)
-					new[j + temp] = av[k][j];
-				new[temp + j] = '\n';
-				temp += (j + 1);
-				k++;
-			}
-			new[temp] = '\0';
+			for (j = 0; av[k][j] != '\0'; j++)
+				new[j + temp] = av[k][j];
+			new[temp + j] = '\n';
+			temp += (j + 1);
+			k++;
 		}
-		else
-		{
-			return (NULL);
-		}
-		return (new);
+		new[temp] = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (new);
 }
