5a8a174fed8f477ff90afefb48738e88be087aea
chriss1525 <okothchris15@gmail.com>
2023-01-21 18:27:17 +0300

    add new node at the begnning of liked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,13 +17,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new->n = n;
-	new->prev = NULL;
 	new->next = (*head);
-	if (head != NULL)
+	if (*head != NULL)
 	{
-		new->prev = (*head);
-		(*head) = new;
+		(*head)->prev = new;
 	}
 
+	(*head) = new;
 	return (*head);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -32,5 +32,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+size_t _print_dlistint_backward(const dlistint_t *h);
 
 #endif /*MAIN_H*/
