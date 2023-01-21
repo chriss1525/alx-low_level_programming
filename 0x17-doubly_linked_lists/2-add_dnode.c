0d277ed3f836b2bde301d635f1787dbd663cc435
chriss1525 <okothchris15@gmail.com>
2023-01-21 17:36:20 +0300

    add new node at the begnning of liked list

---
+++ 0x17-doubly_linked_lists/2-add_dnode.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node to the begining of a list
+ * @head: first element in list
+ * @n: new item in list
+ * Return: new address (Success), NULL (Fail)
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
