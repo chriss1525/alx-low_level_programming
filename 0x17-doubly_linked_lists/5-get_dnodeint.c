01ea0b3a2e8963cf264cbaa9301cec312870484d
chriss1525 <okothchris15@gmail.com>
2023-01-22 19:52:03 +0300

    get nth node in linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -20,10 +20,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 			j = j->next;
 			n++;
 		}
+		else
+		{
+			return (NULL);
+		}
 	}
-	else
-	{
-		return (NULL);
-	}
-	return (head);
+	return (j);
 }
