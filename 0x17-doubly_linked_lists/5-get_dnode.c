f33ef179e9718cf24ba414ca8cfaf8ac4af73461
chriss1525 <okothchris15@gmail.com>
2023-01-22 19:27:05 +0300

    getting the nth node in a linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnode.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns nth node of a linked list
+ * @head: start of linked list
+ * @index: the index of each node
+ * Return: nth node of a linked list (Success), NULL (fail)
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int n = 0;
+	
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (head)
+	{
+		while (n < index)
+		{
+			head = head->next;
+			n++;
+		}
+
+		if (n == index)
+		{
+			return (head);
+		}
+	}
+	return (NULL);
+}
