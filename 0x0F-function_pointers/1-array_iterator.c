d65f34fe01723406695539b1d42600834e91d78a
chriss1525 <okothchris15@gmail.com>
2023-02-27 15:48:24 +0300

    iterate through an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,14 +5,14 @@
  * @array: arra to be iterated
  * @size: size of the array
  * @action: funtion pointer
-*/
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned long i;
+	unsigned long i;
 
-    for (i = 0; i < size; i++)
-    {
-        (*action)(array[i]);
-    }
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
 }
