ba2b992f9142bd16b4e2de5d43a005056a37d768
chriss1525 <okothchris15@gmail.com>
2023-02-27 15:52:26 +0300

    iterate through a string

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
- * @array: arra to be iterated
+ * array_iterator - executes a functionon each element in an array
+ * @array: array to be iterated
  * @size: size of the array
  * @action: funtion pointer
  */
@@ -11,8 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && (*action) != NULL)
 	{
-		(*action)(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
 	}
 }
