a280a15928028ac1f203b316c6c6262d386ab9f3
chriss1525 <okothchris15@gmail.com>
2023-02-27 23:56:41 +0300

    function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,21 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void _printnum(int n);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+int count_place(int n);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_c(va_list f);
+void print_i(va_list f);
+void print_f(va_list f);
+void print_s(va_list f);
+
+
+typedef struct identifier {
+	char *format;
+	void (*print)(va_list f);
+} identifier;
 
 #endif /*MAIN_H*/
