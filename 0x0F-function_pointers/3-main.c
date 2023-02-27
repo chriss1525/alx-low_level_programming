81d95a187f60c59063343cbdaea61fc05f3d672c
chriss1525 <okothchris15@gmail.com>
2023-02-27 17:32:03 +0300

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point to the program that performs simple operations
+ *
+ * @argc: number of command line arguements passed when executing the program
+ * @argv: pointer to array of command line arguements
+ *
+ * Return: Always 0 for success
+ */
+int main(int argc, char *argv[])
+{
+    int (*op_func)(int, int);
+    int ans;
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    op_func = get_op_func(argv[2]);
+
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    ans = op_func(atoi(argv[1]), atoi(argv[3]));
+
+    printf("%d\n", ans);
+    return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int, sum of a and b
+ */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - subtracts a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int, subtracts a and b
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - multiplies a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int, multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div- divides a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int, division of a and b
+ */
+int op_div(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a / b);
+}
+
+/**
+ * op_mod - returns remainder for when a and b are divided
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int, remainder from the division of a and b
+ */
+int op_mod(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a % b);
+}
