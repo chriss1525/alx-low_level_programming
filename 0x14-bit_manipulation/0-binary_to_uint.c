d68688140bc9eb2eac94975fb905422134bc7f2c
chriss1525 <okothchris15@gmail.com>
2023-03-02 09:34:40 +0300

    turn binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int x = 0;
 	int i = 0;
 
-	while (b[i])
-	{
-		x = ((2 * x) + (b[i] - '0'));
-		i++;
-	}
-	if ((b[i] != 0) & (b[i] != 1))
-		return (0);
 	if (b == NULL)
 		return (0);
+	while (b[i])
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			x = ((2 * x) + (b[i] - '0'));
+		}
+		else
+			return (0);
+		i++;
+	}
 	return (x);
 }
