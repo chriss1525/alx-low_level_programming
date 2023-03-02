2733a3d55a43dafc8148c67d36ce5fdd98cbb130
chriss1525 <okothchris15@gmail.com>
2023-03-02 09:53:36 +0300

    fix betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number to change
- * @index: index of the bit to set to 1
+ * get_bit - gets a bit at a given index
+ * @n: number to get the bit for
+ * @index: index of the bit to
  *
  * Return: 1 (success), -1 (failure)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-    if ((n & (1 << index)) == 0)
-        return (0);
+	if ((n & (1 << index)) == 0)
+		return (0);
 
-    return (1);
+	return (1);
 }
