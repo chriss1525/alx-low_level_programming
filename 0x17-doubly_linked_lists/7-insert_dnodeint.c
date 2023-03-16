ac949f5512f3126288bc45dfb4a7b52d7f24e7a8
chriss1525 <okothchris15@gmail.com>
2023-03-17 00:26:14 +0300

    add node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,41 +10,31 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *tmp = *h;
 	dlistint_t *new;
-	dlistint_t *temp;
-	unsigned int i;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 
-	i = 0;
 	new->n = n;
-
-	if (!*h && idx == 0)
-	{
-		new->next = NULL;
-		new->prev = NULL;
-		*h = new;
-	}
-	else
-	{
-		temp = *h;
-
-		while (temp && i < idx)
-		{
-			temp = temp->next;
-			i++;
-		}
-
-		if (!*h)
-			return (NULL);
-
-		temp->prev->next = new;
-		new->prev = temp->prev;
-		temp->prev = new;
-		new->next = temp;
-	}
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
 	return (new);
 }
