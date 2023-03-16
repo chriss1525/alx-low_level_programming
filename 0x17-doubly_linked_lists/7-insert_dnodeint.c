db1965adfa61975422c57b0258d189105cb2cd6b
chriss1525 <okothchris15@gmail.com>
2023-03-17 00:04:22 +0300

    insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,40 +9,38 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new = malloc(sizeof(dlistint_t));
-    dlistint_t *temp;
-    unsigned int i = 0;
-    new->n = n;
-    new->next = NULL;
-    new->prev = NULL;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *temp;
+	unsigned int i = 0;
 
-    if (!new)
-        return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
 
-    if (*h == NULL && idx == 0)
-    {
-        new->next = NULL;
-        new->prev = NULL;
-        *h = new;
-    }
+	if (!new)
+		return (NULL);
 
-    else
-    {
-        temp = *h;
+	if (*h == NULL && idx == 0)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		*h = new;
+	}
 
-        while (temp && i < idx)
-        {
-            temp = temp->next;
-            i++;
-        }
+	else
+	{
+		temp = *h;
 
-        if (*h == NULL)
-            return (NULL);
+		while (temp && i < idx)
+		{
+			temp = temp->next;
+			i++;
+		}
 
-        temp->prev->next = new;
-        new->prev = temp->prev;
-        temp->prev = new;
-        new->next = temp;
-    }
-    return (NULL);
+		temp->prev->next = new;
+		new->prev = temp->prev;
+		temp->prev = new;
+		new->next = temp;
+	}
+	return (NULL);
 }
