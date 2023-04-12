c5f006fe523c029f80cfc96f68f97ae598cb4793
chriss1525 <okothchris15@gmail.com>
2023-04-13 01:56:37 +0300

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash table
+ * @ht: hashtable
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long i;
+	hash_node_t *current;
+	char dict[100] = "";
+	char tmp[100] = "";
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			sprintf(tmp, "%s: %s, ", current->key, current->value);
+			strcat(dict, tmp);
+			current = current->next;
+		}
+	}
+	if (strlen(dict) > 2)
+	{
+		dict[strlen(dict) - 2] = '\0';
+	}
+	printf("{%s}\n", dict);
+}
