251373aa99864416665e2e8321156ae4a9739d35
chriss1525 <okothchris15@gmail.com>
2023-04-12 22:30:25 +0300

    add element to list

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -10,18 +10,9 @@
  */
 int main(void)
 {
-    char *s;
-    unsigned long int hash_table_array_size;
+    hash_table_t *ht;
 
-    hash_table_array_size = 1024;
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "betty", "cool");
     return (EXIT_SUCCESS);
 }
