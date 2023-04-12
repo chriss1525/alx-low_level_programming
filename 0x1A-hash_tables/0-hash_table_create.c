14a1106220cdaec686d77c5093cc7028b3c377cf
chriss1525 <okothchris15@gmail.com>
2023-04-12 21:12:20 +0300

    fix Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *array = malloc(sizeof(hash_table_t));
-    if (!array)
-    {
-        return (NULL);
-    }
-    array->size = size;
-    array->array = malloc(sizeof(hash_node_t *) * size);
-    if (!array->array)
-    {
-        return (NULL);
-    }
-    while (size--)
-    {
-        array->array[size] = NULL;
-    }
-    return array;
+	hash_table_t *array = malloc(sizeof(hash_table_t));
+
+	if (!array)
+	{
+		return (NULL);
+	}
+	array->size = size;
+	array->array = malloc(sizeof(hash_node_t *) * size);
+	if (!array->array)
+	{
+		return (NULL);
+	}
+	while (size--)
+	{
+		array->array[size] = NULL;
+	}
+	return (array);
 }
