3fa9621b3d285718768c326cd3f33ec6dfcf1d96
chriss1525 <okothchris15@gmail.com>
2023-05-02 21:12:17 +0300

    create new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,20 +7,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    FILE *fp;
-    if (filename == NULL)
-    {
-        return -1;
-    }
-    fp = fopen(filename, "w");
-    if (fp == NULL)
-    {
-        return -1;
-    }
-    if (text_content != NULL)
-    {
-        fprintf(fp, "%s", text_content);
-    }
-    fclose(fp);
-    return 1;
+	FILE *fp;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fp = fopen(filename, "w");
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		fprintf(fp, "%s", text_content);
+	}
+	fclose(fp);
+	return (1);
 }
